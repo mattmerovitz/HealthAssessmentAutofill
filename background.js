12bff2df7b27a6a2160e1deebd0d4db4091d869f
mattmerovitz <matthew.merovitz1@gmail.com>
2020-09-03 19:06:33 -0400

    Fixed asking about assessment on schedule page

--- background.js
+++ background.js
@@ -5,10 +5,16 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19')) {
+  if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19') && !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=scheduling')) {
     var conf = confirm("Would you like me to complete the daily health assessment for you?")
     if (conf) {
       chrome.tabs.executeScript(tab.id,{file: "Open.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
     }
+  }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status == 'complete' && tab.url.includes('https://shibboleth.brandeis.edu/idp/profile/SAML2/Redirect/')) {
+    chrome.tabs.executeScript(tab.id,{file: "login.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
   }
 });
---
+++ login.js
@@ -0,0 +1,2 @@
+//setTimeout(function(){ document.getElementById('username').click(); }, 500);
+//setTimeout(function(){ document.getElementsByClassName("form-element form-button btn btn-info btn-block")[0].click(); }, 1000);
