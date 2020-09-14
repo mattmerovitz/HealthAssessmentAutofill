095f41b3123986f62d6f6b1041b82df542382b12
mattmerovitz <matthew.merovitz1@gmail.com>
2020-09-14 14:00:01 -0400

    fine tuning stuff

--- background.js
+++ background.js
@@ -1,20 +1,18 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/register/brandeis_dailyassessment?person=')) {
-    chrome.tabs.executeScript(tab.id,{file: "Autofill.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
-  }
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19') && !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=scheduling')) {
+  if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19') &&
+  !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=scheduling') &&
+  !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=passport') &&
+  !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=training') &&
+  !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=history') &&
+  !tab.url.includes('https://slate.brandeis.edu/portal/brandeis_covid19?cmd=settings')) {
     var conf = confirm("Would you like me to complete the daily health assessment for you?")
     if (conf) {
       chrome.tabs.executeScript(tab.id,{file: "Open.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
+      chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if (changeInfo.status == 'complete' && tab.url.includes('https://slate.brandeis.edu/register/brandeis_dailyassessment?person=') && conf) {
+          chrome.tabs.executeScript(tab.id,{file: "Autofill.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
+        }
+      });
     }
-  }
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status == 'complete' && tab.url.includes('https://shibboleth.brandeis.edu/idp/profile/SAML2/Redirect/')) {
-    chrome.tabs.executeScript(tab.id,{file: "login.js"},function(results){chrome.tabs.update(tab.id,{active:true});});
   }
 });
--- login.js
+++
@@ -1,2 +0,0 @@
-//setTimeout(function(){ document.getElementById('username').click(); }, 500);
-//setTimeout(function(){ document.getElementsByClassName("form-element form-button btn btn-info btn-block")[0].click(); }, 1000);
